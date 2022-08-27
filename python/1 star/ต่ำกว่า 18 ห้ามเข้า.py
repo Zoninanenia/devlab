9ca55bd037c368f5f6f153079d09be4d73ab9c65
zn <111701121+Zoninanenia@users.noreply.github.com>
2022-08-28 01:02:11 +0700

    Update ต่ำกว่า 18 ห้ามเข้า.py

--- python/1 star/ต่ำกว่า 18 ห้ามเข้า.py
+++ python/1 star/ต่ำกว่า 18 ห้ามเข้า.py
@@ -2,5 +2,5 @@ name = input()
 birth = int(input())
 age = (2021 - birth)
 if age >= 18 :
-  print("Welcome " + str(name) + " NongGipsy Pub")
+  print("Welcome " + str(name) + " to NongGipsy Pub")
 else:print("You shall not pass!")
